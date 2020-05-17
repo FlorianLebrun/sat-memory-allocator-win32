dfe8d9e0cd44f606df19b19ca2db5a63123a230e
FlorianLebrun <florian.lebrun.pv1@gmail.com>
2020-05-17 10:10:39 +0200

    use shared cmake tools

--- .tools/certificate/make-test-certificate.ps1
+++
@@ -1,4 +0,0 @@
-
-$cert = New-SelfSignedCertificate -DnsName "Open Source" -Type CodeSigning -CertStoreLocation Cert:\CurrentUser\My
-$CertPassword = ConvertTo-SecureString -String "my_password" -Force -AsPlainText
-Export-PfxCertificate -Cert "cert:\CurrentUser\My\$($cert.Thumbprint)" -FilePath "$($PSScriptRoot)\build-certificate.pfx" -Password $CertPassword
--- .tools/index.cmake
+++
@@ -1,6 +0,0 @@
-
-include(.tools/utils.cmake)
-include(.tools/projects/targets.cmake)
-include(.tools/projects/sources.cmake)
-include(.tools/nodejs/index.cmake)
-include(.tools/packaging/index.cmake)
